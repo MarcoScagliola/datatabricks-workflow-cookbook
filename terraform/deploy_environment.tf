f9c9d467c87ec6b11be220cc47b01e9cb8866ec4
Marco <marco.scagliola@databricks.com>
2024-05-03 23:52:16 +0100

    01-Deploy Azure Environment

--- terraform/deploy_environment.tf
+++ terraform/deploy_environment.tf
@@ -22,7 +22,7 @@ terraform {
   required_version = "= 1.8.0"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.102.0"
     }
   }
